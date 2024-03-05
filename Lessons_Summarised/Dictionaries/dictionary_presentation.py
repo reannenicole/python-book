e267b82d4639ad5066aab893d5ac80ea2d17cd66
Reanne Nicole Pearce <reannenicolep@gmail.com>
2024-03-05 07:19:36 +0000

    expanded dictionaries

--- Lessons_Summarised/Dictionaries/dictionary_presentation.py
+++ Lessons_Summarised/Dictionaries/dictionary_presentation.py
@@ -9,3 +9,25 @@
 mobster_0 = {"build": "athletic", "points": "7"}
 print(mobster_0["build"])
 print(mobster_0["points"])
+
+# you can add new values to a dictionary after the fact, and even create a blank dictionary
+# to add stuff to in the future (think user submitted data, list of scores, etc)
+
+mobster_1 = {}
+print (mobster_1)
+
+mobster_1["build"] = "chubby"
+mobster_1["points"] = "9"
+
+print(mobster_1)
+print(mobster_1["build"])
+
+new_points = mobster_1["points"]
+
+print(f"If you kill Pauly, you earn {new_points} points!")
+
+# You can even modify the value of a key after the fact
+
+mobster_1["alive"] = "yes"
+print(f"The mobster is currently {mobster_1["alive"]}.")
+
